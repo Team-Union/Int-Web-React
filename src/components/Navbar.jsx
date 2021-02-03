c911111d3fbb96ad7cd126eb8b682b3b7f9e05e0
MadeGOD <anhgerelanhaa87@gmail.com>
2021-02-03 14:38:42 +0800

    UI 2차 작업

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,38 @@ import React from "react";
 
 const NavBar = props => {
   return (
-    <div className="ui inverted container">
-      <nav>
-        <div className="ui inverted secondary menu">
-          <a className="ui active item" href="/">
-            <img src="/logo192.png" alt="Logo" width="100" />
-            Team Int
-          </a> 
-          <div className="right menu">
-            <div className="item">
-              <a className="circular ui inverted button" href="/discord">
-                디스코드 서버
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <nav class="navbar navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Team Int</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">홈</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/discord">디스코드 서버</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                팀(Team)
               </a>
-            </div>
-          </div>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/about">소개</a></li>
+                <li><a class="dropdown-item" href="/members">팀원</a></li>
+                <li><a class="dropdown-item" href="/join">채용</a></li> 
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">대시보드</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
